1bfae691ea95e6a2ed99268b46599f8658e21af3
Valdo <83594809+ValdoGudino@users.noreply.github.com>
2024-12-01 02:43:37 -0600

    Day One Solved

---
+++ 2024/day-one/day-one.py
@@ -0,0 +1,22 @@
+print('Calculating distance between two lists of numbers...')
+first_list = []
+second_list = []
+
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        num1, num2 = map(int, line.split())
+        first_list.append(num1)
+        second_list.append(num2)
+        
+
+first_list.sort()
+second_list.sort()
+
+distance = sum(abs(a - b) for a, b in zip(first_list, second_list))
+print("Distance:", distance)
+
+print('Calculating the similarity score between two lists of numbers...')
+
+similarity_score = sum(num * second_list.count(num) for num in first_list)
+print("Similarity Score:", similarity_score)
