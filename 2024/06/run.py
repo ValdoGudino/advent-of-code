c26df76373f22962e0f9559b8be7951c8799e282
Valdo <83594809+ValdoGudino@users.noreply.github.com>
2024-12-06 01:15:15 -0600

    Use path to better performance

--- 2024/06/run.py
+++ 2024/06/run.py
@@ -1,3 +1,7 @@
+import time
+
+start_time = time.time()
+
 from utils.utils import read_grid_from_file
 
 directions = {'^': (-1, 0), '>': (0, 1), 'v': (1, 0), '<': (0, -1)}
@@ -15,7 +19,6 @@ def simulate_guard_movement(grid):
         for j, cell in enumerate(row):
             if cell in directions:
                 x, y = i, j
-                grid[x][y] = '.'
                 direction = cell
                 break
 
@@ -29,7 +32,7 @@ def simulate_guard_movement(grid):
         
         if out_of_bounds(nx, ny, grid): 
             break
-        elif grid[nx][ny] == '.':
+        elif grid[nx][ny] == '.' or grid[nx][ny] in directions:
             x, y = nx, ny
         else:
             if (nx, ny, direction) in obstacles_encountered:
@@ -37,7 +40,7 @@ def simulate_guard_movement(grid):
             obstacles_encountered[(nx, ny, direction)] = True
             direction = turns[direction]
     
-    return len(visited), False
+    return len(visited), False, visited
 
 def count_obstacle_positions(grid):
     count = 0
@@ -49,25 +52,28 @@ def count_obstacle_positions(grid):
                 initial_x, initial_y = i, j
                 break
 
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == '.' and (i, j) != (initial_x, initial_y):
-                grid_copy = [row[:] for row in grid]  # Create a deep copy of the grid
-                grid_copy[i][j] = '#'
-                _, is_loop = simulate_guard_movement(grid_copy)
-                if is_loop:
-                    count += 1
-                    print("Obstacle at position", i, j, "causes a loop")
-                grid_copy[i][j] = '.'
+    path = simulate_guard_movement(grid)[2]
+
+    for x, y in path:
+        if grid[x][y] == '.' and (x, y) != (initial_x, initial_y):
+            grid_copy = [row[:] for row in grid] 
+            grid_copy[x][y] = '#'
+            is_loop = simulate_guard_movement(grid_copy)[1]
+            if is_loop:
+                count += 1
+            grid_copy[x][y] = '.'
     return count
 
 print("Starting the guard simulation...")
 input_string = read_grid_from_file()
 grid = parse_grid(input_string)
-grid_copy = [row[:] for row in grid]
-distinct_coordinates, is_loop = simulate_guard_movement(grid_copy)
+distinct_coordinates, is_loop, _ = simulate_guard_movement(grid)
 print("The number of distinct coordinates visited by the guard is:", distinct_coordinates)
 
 grid_copy = [row[:] for row in grid]
-obstacle_positions = count_obstacle_positions(grid_copy)
+obstacle_positions = count_obstacle_positions(grid)
 print("The number of ways to add an obstacle to get the guard stuck in a loop is:", obstacle_positions)
+
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"Execution time: {execution_time:.2f} seconds")
