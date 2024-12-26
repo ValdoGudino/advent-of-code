7d8d308c5f8f8e8c8d6a22c7549e158371137f8e
Valdo <83594809+ValdoGudino@users.noreply.github.com>
2024-12-25 22:13:36 -0600

    Fuck Off Day 9

--- 2024/09/run.py
+++ 2024/09/run.py
@@ -33,6 +33,48 @@ def move_blocks(parsed_list):
     
     return parsed_list
 
+def find_file_blocks(parsed_list):
+    blocks = []
+    i = 0
+    while i < len(parsed_list):
+        if parsed_list[i].isdigit():
+            start = i
+            while i < len(parsed_list) and parsed_list[i] == parsed_list[start]:
+                i += 1
+            blocks.append((parsed_list[start], start, i - start))
+        else:
+            i += 1
+    return blocks
+
+def find_free_spaces(parsed_list):
+    free_spaces = []
+    i = 0
+    while i < len(parsed_list):
+        if parsed_list[i] == '.':
+            start = i
+            while i < len(parsed_list) and parsed_list[i] == '.':
+                i += 1
+            free_spaces.append((start, i - start))
+        else:
+            i += 1
+    return free_spaces
+
+def move_whole_blocks(parsed_list):
+    blocks = find_file_blocks(parsed_list)
+    free_spaces = find_free_spaces(parsed_list)
+    
+    for block in reversed(blocks):
+        block_char, block_start, block_length = block
+        for space in free_spaces:
+            space_start, space_length = space
+            if space_length >= block_length and space_start < block_start:
+                parsed_list[space_start:space_start + block_length] = [block_char] * block_length
+                parsed_list[block_start:block_start + block_length] = ['.'] * block_length
+                free_spaces = find_free_spaces(parsed_list)
+                break
+
+    return parsed_list
+
 def calculate_checksum(parsed_list):
     checksum = 0
     for index, value in enumerate(parsed_list):
@@ -41,6 +83,10 @@ def calculate_checksum(parsed_list):
     return checksum
 
 parsed_list = load_and_parse()
-result_list = move_blocks(parsed_list)
+result_list = move_blocks(parsed_list.copy())
 checksum = calculate_checksum(result_list)
 print(checksum)
+
+whole_result_list = move_whole_blocks(parsed_list.copy())
+whole_checksum = calculate_checksum(whole_result_list)
+print(whole_checksum)
